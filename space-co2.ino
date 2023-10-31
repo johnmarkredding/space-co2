2a4d7e7fe3f1c792caa412e52a312c59713f044e
John Mark Redding <johomurk@gmail.com>
2023-10-30 19:47:54 -0700

    Remove unnecessary comments, and fix indentation

--- space-co2.ino
+++ space-co2.ino
@@ -22,9 +22,7 @@ void setup() {
 }
 
 void loop() {
-  
   // CO2 STUFFS
-  // Read CO2 concentration via UART
 
   if (co2sensor.isPreHeating()) {
     // Here display "preheat" on screen until we have a value.
@@ -42,30 +40,26 @@ void loop() {
     }
   }
 
-    // Always print latest reading.
-    Serial.print("Latest co2 concentration: ");
-    Serial.println(latestPPM);
+  // Always print latest reading.
+  Serial.print("Latest co2 concentration: ");
+  Serial.println(latestPPM);
+
 
   // BAROMETER STUFFS
 
-  // read the sensor value
   float pressure = BARO.readPressure();
 
-  // print the sensor pressure
   Serial.print("Pressure = ");
   Serial.print(pressure);
   Serial.println(" kPa");
 
   float temperature = BARO.readTemperature();
 
-  // print the sensor temp
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" C");
-
-  // print an empty line
   Serial.println();
 
-  // wait 1 second to print again
+  // only run loop once every second
   delay(1000);
 }
