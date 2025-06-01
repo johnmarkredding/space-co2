7489ffc8ca7ae3d1d4ca633849693f34fb1d0d72
John Mark <johomurk@gmail.com>
2025-06-01 22:45:56 +0000

    Fix formatting issues committed by contributor.

--- space-co2.ino
+++ space-co2.ino
@@ -8,10 +8,10 @@ U8X8_SSD1309_128X64_NONAME0_4W_HW_SPI u8x8(/* cs=*/ 8, /* dc=*/ 9, /* reset=*/ 1
 
 MHZ co2sensor(&Serial1, MHZ19B);
 
-bool CO2_ALARM = false; //marks state of excess CO2 for sounding alarm, etc.
-bool ALARM_CANCEL = false; //marks state of alarm cancellation
-const int BUTTON_PIN = 2; // the number of the pushbutton pin
-const int BUZZER_PIN = 3; // the number of the buzzer pin
+bool CO2_ALARM = false;         // marks state of excess CO2 for sounding alarm, etc.
+bool ALARM_CANCEL = false;      // marks state of alarm cancellation
+const int BUTTON_PIN = 2;       // the number of the pushbutton pin
+const int BUZZER_PIN = 3;       // the number of the buzzer pin
 const int ALARM_CANCEL_PIN = 4; // the number of the button to cancel CO2 level alarm
 const int calibrate_button_time = 7;
 
@@ -33,38 +33,36 @@ int PPM_scaled;
 int PPM_raw;
 int calibrated_ind_count;
 int cal_ind_disp_lngth = 5;
-int CO2_Concen_Threshold = 1000; //Set CO2 alarm concentration threshold here.
+int CO2_Concen_Threshold = 1000; // Set CO2 alarm concentration threshold here.
 int refresh_rate = 1000;
-int ALARM_TIME = 60; //Set alarm cancel duration here.
+int ALARM_TIME = 60; // Set alarm cancel duration here.
 int ALARM_COUNT = ALARM_TIME;
-
 
 AT24C02 eprom(AT24C_ADDRESS_0);
 
 void setup() {
-  
-  digitalWrite(BUZZER_PIN, HIGH);  // sets BUZZER_PIN HIGH to turn off piezo buzzer
-  
+
+  digitalWrite(BUZZER_PIN, HIGH); // sets BUZZER_PIN HIGH to turn off piezo buzzer
+
   Serial.begin(9600);
   Serial1.begin(9600);
 
- // while (!Serial);
- // while (!Serial1);
-  
+  // while (!Serial);
+  // while (!Serial1);
+
   // Set up AT24C02 EEPROM
 
   Wire.begin();
   uint8_t data = eprom.read(0);
-// Check for error
+  // Check for error
   eprom.get(0, reference_pressure);
   Serial.print(reference_pressure);
   if (eprom.getLastError() != 0) {
-  Serial.print("Error reading from eeprom");
-  u8x8.drawString(0, 4, "EEPROM Error");
-  } 
+    Serial.print("Error reading from eeprom");
+    u8x8.drawString(0, 4, "EEPROM Error");
+  }
 
-
-// Initialize barometer
+  // Initialize barometer
   if (!BARO.begin()) {
     Serial.println("Failed to initialize pressure sensor!");
     while (1);
@@ -76,9 +74,9 @@ void setup() {
 
   // Ensure the calibration is NOT automatic and then calibrate from given environment. WILL BE CHANGED LATER.
   co2sensor.setAutoCalibrate(false);
-  //co2sensor.calibrateZero();
+  // co2sensor.calibrateZero();
 
-  //Set digital pin pull up resistor mode
+  // Set digital pin pull up resistor mode
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(ALARM_CANCEL_PIN, INPUT_PULLUP);
@@ -88,16 +86,17 @@ void loop() {
   // Check if the calibration button is pressed for seven cycles, and then calibrate CO2 meter and record current barometric
   // pressure for concentration scaler
   button_state = digitalRead(BUTTON_PIN);
-  if(button_state == LOW){
+  if (button_state == LOW) {
     Serial.println("Button State Low");
-    if(calibrate_count < calibrate_button_time){
-      calibrate_count ++;
-  }}
-  else if (button_state == HIGH) {
+    if (calibrate_count < calibrate_button_time) {
+      calibrate_count++;
+    }
+  } else if (button_state == HIGH) {
     Serial.println("Button State High");
     calibrate_count = 0;
   }
-  if(calibrate_count == calibrate_button_time){
+  
+  if (calibrate_count == calibrate_button_time) {
     co2sensor.calibrateZero();
     current_pressure = BARO.readPressure();
     reference_pressure = BARO.readPressure();
@@ -106,14 +105,13 @@ void loop() {
     eprom.put(0, reference_pressure);
   }
 
-
   // CO2 STUFFS
   if (co2sensor.isPreHeating()) {
     // Here display "preheat" on screen until we have a value.
     latestPPM = "Preheating";
   } else if (co2sensor.isReady()) {
     int co2_ppm = co2sensor.readCO2UART();
-    
+
     // Setup screen for new value
     u8x8.clear();
     if (co2_ppm >= 0) {
@@ -135,7 +133,7 @@ void loop() {
 
   // BAROMETER STUFFS
 
-  //float pressure = BARO.readPressure();
+  // float pressure = BARO.readPressure();
   latestPressure = arduino::String(BARO.readPressure());
   latestPressure += " kPa";
   current_pressure = (BARO.readPressure());
@@ -149,8 +147,8 @@ void loop() {
   Serial.print("Temperature = ");
   Serial.println(latestTemp);
 
-// Calculate CO2 concentration scaler to compensate for changes in barometric pressure.
-  concentration_scaler = reference_pressure/current_pressure;
+  // Calculate CO2 concentration scaler to compensate for changes in barometric pressure.
+  concentration_scaler = reference_pressure / current_pressure;
 
   Serial.print("Concentration scaler = ");
   Serial.println(concentration_scaler);
@@ -159,64 +157,64 @@ void loop() {
   concen_scaler_disp += " Scaler";
   std::string reference_pressure_disp = std::to_string(reference_pressure);
 
-//Scale the CO2 measurement to compensate for changes in pressure, and convert it to a string for display.
+  // Scale the CO2 measurement to compensate for changes in pressure, and convert it to a string for display.
   PPM_scaled = PPM_raw * concentration_scaler;
   std::string PPM_scaled_str = std::to_string(PPM_scaled);
   PPM_scaled_str += "ppms";
 
-// If calibration has occured, decrement the counter for how long to display "calibrated" on the screen. Once time out, set to blank. 
-if (calibrated_ind_count >= 1){
- calibrated_ind_count --;
-}
- if (calibrated_ind_count < 1){
-  calibrated_ind = "          ";
- }
+  // If calibration has occured, decrement the counter for how long to display "calibrated" on the screen. Once time out, set to blank.
+  if (calibrated_ind_count >= 1) {
+    calibrated_ind_count--;
+  }
+  if (calibrated_ind_count < 1) {
+    calibrated_ind = "          ";
+  }
 
   reference_pressure_disp += " REF";
 
-  //Handle alarm conditions for excess CO2 concentration
-  //Check if CO2 level is above maximum safe threshold.
-  if(PPM_scaled >= CO2_Concen_Threshold){
+  // Handle alarm conditions for excess CO2 concentration
+  // Check if CO2 level is above maximum safe threshold.
+  if (PPM_scaled >= CO2_Concen_Threshold) {
     CO2_ALARM = true;
   }
-// Reset CO2 alarm if level falls below threshold
-  if(PPM_scaled < CO2_Concen_Threshold){
+  // Reset CO2 alarm if level falls below threshold
+  if (PPM_scaled < CO2_Concen_Threshold) {
     CO2_ALARM = false;
   }
-// Start Buzzer Alarm
-if (CO2_ALARM == true && ALARM_CANCEL == false){
-  digitalWrite(BUZZER_PIN, LOW);
+  // Start Buzzer Alarm
+  if (CO2_ALARM == true && ALARM_CANCEL == false) {
+    digitalWrite(BUZZER_PIN, LOW);
     Serial.print("Alarm on and not cancelled");
-}
-else {digitalWrite(BUZZER_PIN, HIGH);} //turns off alarm automatically if concentration drops below threshold
+  } else {
+    digitalWrite(BUZZER_PIN, HIGH);
+  } // turns off alarm automatically if concentration drops below threshold
 
-//Check for alarm cancel button push
-cancel_button_state = digitalRead(ALARM_CANCEL_PIN);
-Serial.print("Alarm Cancel button State: ");
-Serial.println(cancel_button_state);
-if(cancel_button_state == LOW){
-ALARM_CANCEL = true;
+  // Check for alarm cancel button push
+  cancel_button_state = digitalRead(ALARM_CANCEL_PIN);
+  Serial.print("Alarm Cancel button State: ");
+  Serial.println(cancel_button_state);
+  if (cancel_button_state == LOW) {
+    ALARM_CANCEL = true;
     Serial.println("Alarm cancelled");
-}
+  }
 
-
-if (ALARM_CANCEL == true && CO2_ALARM == true){
-digitalWrite(BUZZER_PIN, HIGH);
-ALARM_COUNT --;
+  if (ALARM_CANCEL == true && CO2_ALARM == true) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    ALARM_COUNT--;
     Serial.print("ALARM_COUNT :");
     Serial.println(ALARM_COUNT);
-}
-if (ALARM_COUNT <= 0){
-  ALARM_CANCEL = false;
-  ALARM_COUNT = ALARM_TIME;
-  Serial.print("Alarm cancellation ended");
-}
+  }
+  if (ALARM_COUNT <= 0) {
+    ALARM_CANCEL = false;
+    ALARM_COUNT = ALARM_TIME;
+    Serial.print("Alarm cancellation ended");
+  }
 
   // only run loop once every second
   delay(refresh_rate);
 
-    // Print to display
-  //u8x8.clear();
+  // Print to display
+  // u8x8.clear();
   u8x8.drawString(0, 0, latestPPM.c_str());
   u8x8.drawString(0, 1, PPM_scaled_str.c_str());
   u8x8.drawString(0, 2, latestPressure.c_str());
@@ -224,12 +222,9 @@ if (ALARM_COUNT <= 0){
   u8x8.drawString(0, 4, latestTemp.c_str());
   u8x8.drawString(0, 7, calibrated_ind.c_str());
 
- // u8x8.drawString(0, 4, concen_scaler_disp.c_str());
+  // u8x8.drawString(0, 4, concen_scaler_disp.c_str());
 
- //buzz for 1/8 of a second
-  //digitalWrite(BUZZER_PIN, LOW); // sets the digital pin 13 on
-  //delay(125);            // waits for a second
-
-
-
+  // buzz for 1/8 of a second
+  // digitalWrite(BUZZER_PIN, LOW); // sets the digital pin 13 on
+  // delay(125);            // waits for a second
 }
